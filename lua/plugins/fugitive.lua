f2a39546e61eb8145065df21477372b1bc6fba7a
Jesus Lopez <jesus@jesusla.com>
2024-03-20 20:50:35 -0700

    Clean up fugitive startup

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,35 +1,16 @@
 return {
     'tpope/vim-fugitive',
-    keys = {
-        { '<leader>gs', '<cmd>Git<cr>' },
+    dependencies = {
+        'tpope/vim-rhubarb',
+        'cedarbaum/fugitive-azure-devops.vim',
     },
-    config = function()
-        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
-
-        local autocmd = vim.api.nvim_create_autocmd
-        autocmd("BufWinEnter", {
-            group = ThePrimeagen_Fugitive,
-            pattern = "*",
-            callback = function()
-                if vim.bo.ft ~= "fugitive" then
-                    return
-                end
-
-                local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
-                vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git('push')
-                end, opts)
-
-                -- rebase always
-                vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
-                end, opts)
-
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
-            end,
-        })
-    end,
+    keys = {
+        { '<leader>gs', '<cmd>Git<CR>' },
+        { '<leader>gb', '<cmd>GBrowse<CR>' },
+        { '<leader>gb', "<cmd>'<,'>GBrowse<CR>", mode='x' },
+        { '<leader><leader>', '<cmd>GBrowse<CR>', ft='fugitive', buffer=0, remap=false },
+        { '<leader>p', '<cmd>Git push<CR>', ft='fugitive', buffer=0, remap=false },
+        { '<leader>P', '<cmd>Git push --amend<CR>', ft='fugitive', buffer=0, remap=false },
+    },
+    cmd = { 'Git', 'Gbrowse' },
 }
