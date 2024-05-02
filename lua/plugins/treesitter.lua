9caf0579fc263e61c2e122d71fb7706e18d7ee09
Jesus Lopez <jesus@jesusla.com>
2024-05-01 17:11:53 -0700

    fix - use treesitter indentation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -35,6 +35,10 @@ return {
                 -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = false,
             },
+
+            indent = {
+                enable = true,
+            },
         })
     end,
 }
