218cd6e4f7654a432c3876b43fa71ac73507553b
Jesus Lopez <jesus@jesusla.com>
2024-05-06 12:11:59 -0700

    update to latest

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,13 +1,16 @@
 return {
     'rest-nvim/rest.nvim',
+    ft = 'http',
     -- Making the plugin lazy breaks filetype detection
-    -- keys = {
-    --     { '<leader>hr', '<Plug>RestNvim' },
-    --     { '<leader>hp', '<Plug>RestNvimPreview' },
-    -- },
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        { '<leader>rr', '<cmd>Rest run<cr>' },
+        { '<leader>rl', '<cmd>Rest run last<cr>' },
+    },
+    dependencies = { 'luarocks.nvim' },
     config = function()
-        vim.keymap.set("n", "<leader>hr", "<Plug>RestNvim")
-        vim.keymap.set("n", "<leader>hp", "<Plug>RestNvimPreview")
+        require('rest-nvim').setup({
+        });
+        -- vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>")
+        -- vim.keymap.set("n", "<leader>rl", "<Plug>Rest run last<cr>")
     end,
 }
